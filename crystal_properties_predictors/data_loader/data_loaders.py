0ece948f55b33ab4c04daf8ed2ab813d87dd3efb
Tomas Giedraitis <tomasgiedraitis@gmail.com>
2023-10-14 11:30:08 +0300

    Add `CrystalDataLoader` for loading crystal data.

--- crystal_properties_predictors/data_loader/data_loaders.py
+++ crystal_properties_predictors/data_loader/data_loaders.py
@@ -2,6 +2,7 @@ from torch.utils.data import DataLoader
 from torchvision import datasets
 
 from crystal_properties_predictors.base import DataLoaderBase
+from crystal_properties_predictors.dataset import CrystalDataset
 
 
 class MnistDataLoader(DataLoaderBase):
@@ -30,6 +31,50 @@ class MnistDataLoader(DataLoaderBase):
                 self.data_dir,
                 train=False,
                 download=True,
+                transform=transforms.build_transforms(train=False),
+            )
+            if train
+            else None
+        )
+
+        self.init_kwargs = {"batch_size": batch_size, "num_workers": nworkers}
+        super().__init__(
+            self.train_dataset, shuffle=shuffle, **self.init_kwargs
+        )
+
+    def split_validation(self):
+        if self.valid_dataset is None:
+            return None
+        else:
+            return DataLoader(self.valid_dataset, **self.init_kwargs)
+
+
+class CrystalDataLoader(DataLoaderBase):
+    """Load crystal data."""
+
+    def __init__(
+        self,
+        transforms,
+        data_dir,
+        batch_size,
+        shuffle,
+        validation_split,
+        nworkers,
+        train=True,
+    ):
+        self.data_dir = data_dir
+
+        self.train_dataset = CrystalDataset(
+            self.data_dir,
+            train=train,
+            download=False,
+            transform=transforms.build_transforms(train=True),
+        )
+        self.valid_dataset = (
+            CrystalDataset(
+                self.data_dir,
+                train=False,
+                download=False,
                 transform=transforms.build_transforms(train=False),
             )
             if train
